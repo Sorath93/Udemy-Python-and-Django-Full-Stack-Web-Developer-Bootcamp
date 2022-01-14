c817f6faca3c8d6441f42851c33e950b8fc3cb07
Sorath Soomro <Sorath.Soomro@isode.com>
2022-01-14 18:45:28 +0000

    control flow in a function

--- DJANGO_COURSE_2.xx/08-Javascript_Level_Two/Array_exercise_myanswer.js
+++ DJANGO_COURSE_2.xx/08-Javascript_Level_Two/Array_exercise_myanswer.js
@@ -1,55 +1,56 @@
-var roster = []
+let roster = []
 
-while (true) {
-    var startApp = prompt("Would you like to start the roster app? (y/n)?")
-    if (startApp == "y") {
-        var action = prompt("Please select an action: add, remove, display or quit")
-        if (action == "add") {
-            var itemToAdd = prompt("what name would you like  to add?")
-            roster.push(item)
-        }
-
-        else if (action == "remove") {
-            itemToRemove = prompt("What name would you like to remove?")
-            itemToRemoveIndex = roster.findIndex(function () { return item_to_remove })
-            itemToRemove.splice(itemToRemoveIndex)
-        }
-
-        else if (action == "display") {
-            console.log(roster)
-        }
-
-        else {
-            break
-        }
+function cancelled(parameter) {
+    if (parameter == null) {
+        return
     }
-    else {
-        break
-    }
-
 }
 
-function action() {
-    if (startApp == "y") {
-        var action = prompt("Please select an action: add, remove, display or quit")
+function action(action) {
         if (action == "add") {
-            var itemToAdd = prompt("what name would you like  to add?")
-            roster.push(item)
+            var itemToAdd = prompt("what name would you like  to add?")  //check cancel has been pressed here
+            roster.push(itemToAdd)
         }
 
         else if (action == "remove") {
-            itemToRemove = prompt("What name would you like to remove?")
-            itemToRemoveIndex = roster.findIndex(function () { return item_to_remove })
-            itemToRemove.splice(itemToRemoveIndex)
+            //check for an empty list
+            if (roster.length === 0) {
+                alert("There is nothing to remove")
+                return
+            }
+
+            var itemToRemove = prompt("What name would you like to remove?") //check cancel has been pressed here
+            var itemToRemoveIndex = roster.indexOf(itemToRemove)
+            console.log(itemToRemoveIndex)
+            roster.splice(itemToRemoveIndex)
         }
 
         else if (action == "display") {
             console.log(roster)
         }
 
-        else {
+        else if (action != "add" && action != "remove" && action != "display" && action != "quit") {
+            alert("You entered an invalid option")
+        }
+
+        else if  (action == null) {
+            return
+        }
+    }
+
+
+var startApp = prompt("Would you like to start the Roster app? (y/n)")
+
+while (true) {
+    if (startApp == "y") {
+        var actionChosen = prompt("Please select an action: add, remove, display or quit")
+        if (actionChosen == "quit") {
             break
         }
+        else if (actionChosen == null) {
+            break
+        }
+        action(actionChosen)
     }
     else {
         break
